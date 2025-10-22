defd6839eb9f934da9a3ed5cde4849c8afa02176
Efrain Colmenares <efraxpc@gmail.com>
2025-10-21 21:15:58 -0300

    chore: anotação sync/async

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ const app = require('./app');
 const { createConfig } = require('./config/config');
 
 async function execute() {
+    //sincrono
     const configPath = path.join(__dirname, '../configs/.env');
+    //sincrono
     const appConfig = createConfig(configPath);
 
     await db.connect(appConfig);
+    //asincrono
     const server = app.listen(appConfig.port, () => {
         console.log('account service started', { port: appConfig.port });
     });
 
     const closeServer = () => {
         if (server) {
+            //asincrono
             server.close(() => {
                 console.log('server closed');
                 process.exit(1);
@@ -27,7 +31,7 @@ async function execute() {
         console.log('unhandled error', { error });
         closeServer();
     };
-
+    //sincrono
     process.on('uncaughtException', unexpectedError);
     process.on('unhandledRejection', unexpectedError);
 }

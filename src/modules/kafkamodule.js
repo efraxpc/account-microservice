593859e67253f033d234d43ceb3206420f116e58
Efrain Colmenares <efraxpc@gmail.com>
2025-10-25 23:24:54 -0300

    chore: kafkamodule fixed

--- src/modules/kafkamodule.js
+++ src/modules/kafkamodule.js
@@ -1,53 +1,50 @@
 const { Kafka } = require('kafkajs');
 const Account = require('../models/account');
 const path = require('path');
-const { createConfig } = require('../config/config');
+const { createConfig } = require('../config/config')
 
 const configPath = path.join(__dirname, '../../configs/.env');
 const appConfig = createConfig(configPath);
 const kafka = new Kafka({
-  clientId: appConfig.kafka.clientId,
-  brokers: [appConfig.kafka.brokers],
+    clientId: appConfig.kafka.clientId,
+    brokers: [appConfig.kafka.brokers],
 });
 
 const consumer = kafka.consumer({ groupId: appConfig.kafka.groupId });
 
 const consumerModule = async () => {
-  await consumer.connect();
-  await consumer.subscribe({ topic: appConfig.kafka.topic });
+    await consumer.connect();
+    await consumer.subscribe({ topic: appConfig.kafka.topic });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const transaction = JSON.parse(message.value.toString());
-      const accountId = transaction.accountId;
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
 
-      try {
-        const blockedAccount = await Account.findOne({ accountId, status: 'blocked' });
-        if (!blockedAccount) {
-          // Si la cuenta no está bloqueada, intentamos actualizar o crear el contador de fraude
-          const updatedAccount = await Account.findOneAndUpdate(
-            { id: accountId },
-            { $inc: { count: 1 } },
-            { new: true }
-          );
-          console.log(blockedAccount);
+            const transaction = JSON.parse(message.value.toString());
+            const accountId = transaction.accountId;
+            try {
+                const blockedAccount = await Account.findOne({ accountId, status: { $ne: 'blocked' } });
 
-          if (updatedAccount.count === 3) {
-            // Si el contador llega a 3, bloqueamos la cuenta
-            await Account.findOneAndUpdate(
-              { id: accountId },
-              { status: 'blocked' },
-              { new: true }
-            );
-          }
-        } else {
-          console.log(`not a valid accountId ${accountId}`);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  });
+                if (!blockedAccount) {
+                    const updatedAccount = await Account.findOneAndUpdate(
+                        { _id: accountId },
+                        { $inc: { count: 1 } },
+                        { new: true }
+                    );
+                    if (updatedAccount.count === 3)
+                        await Account.findOneAndUpdate(
+                            { _id: accountId },
+                            { status: 'blocked' },
+                            { new: true }
+                        );
+                }
+                else
+                    console.log(`not a valid accountId ${accountId}`);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+    });
 };
 
 module.exports = consumerModule;
